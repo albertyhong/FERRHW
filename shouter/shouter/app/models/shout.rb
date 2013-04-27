5e7cbceb2845f9550effbe1c3649db75b2f1d789
Albert Hong <hong.albert@gmail.com>
2013-04-26 23:54:58 -0400

    Updated Shouter with model validations

--- shouter/shouter/app/models/shout.rb
+++ shouter/shouter/app/models/shout.rb
@@ -1,5 +1,6 @@
 class Shout < ActiveRecord::Base
   attr_accessible :shout_to_id, :text, :user_id
+  validates :text, :presence => true, :length => { :maximum => 144 }
 
   belongs_to :user
 end
--- shouter/shouter/app/models/user.rb
+++ shouter/shouter/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password
+  validates :email, :presence => true
 
   has_many :shouts
   #Even though model name is singular, shouts is still plural because it is many shouts
