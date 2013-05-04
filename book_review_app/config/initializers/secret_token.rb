c0e6c352dc053bb099e54b5b1acea70f670355eb
Albert Hong <hong.albert@gmail.com>
2013-05-04 12:30:28 -0400

    Delete book_review_app

--- book_review_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-BookReviewApp::Application.config.secret_token = 'fb8c8be2b5a19a76e9f258688ca46f85e7300057af9f8bd50ebd3cd1e1b4764ea7f96dd468ed021439ff5358ae7781d7910df038734e1296346b1a06929963af'
